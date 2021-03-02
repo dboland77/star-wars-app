75bdb0bdf5d8d9669a849d19f8c32f144ec84cb6
David Boland <dboland77@gmail.com>
2021-03-02 23:23:17 +0000

    folder tidy up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from "react";
+import { useFetch } from "./hooks/useFetch";
+import * as constants from "./constants/constants";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  let planeturl, filmurl;
+
+  const character = useFetch(
+    `${process.env.REACT_APP_BASE_URL}${constants.PEOPLE_SLUG}20/`,
+    constants.CHARACTER
   );
-}
+
+  // Need to switch http to https to avoid redirect and cors issues
+
+  if (
+    character.status === constants.STATUS_FETCHED &&
+    character.error === null
+  ) {
+    planeturl = character.characterdata.homeworld;
+    planeturl = planeturl.replace(constants.HTTP_RX, constants.HTTPS);
+    filmurl = character.characterdata.films[0];
+    filmurl = filmurl.replace(constants.HTTP_RX, constants.HTTPS);
+    console.log(planeturl, filmurl);
+  }
+
+  // We cannot call useFetch conditionally (rules of hooks)
+  // But we CAN add (!url return) to the useFetch and condional the url parameter!!!!
+  // Thank you Kent C. Dodds :)
+
+  const planet = useFetch(
+    character.status === constants.STATUS_FETCHED && character.error === null
+      ? planeturl
+      : null,
+    constants.PLANET
+  );
+
+  const film = useFetch(
+    character.status === constants.STATUS_FETCHED && character.error === null
+      ? filmurl
+      : null,
+    constants.FILM
+  );
+
+  return (
+    <Fragment>
+      {character.status === constants.STATUS_FETCHED &&
+        character.error === null && (
+          <h1>
+            Name: {character.characterdata.name} <br/>
+            Birth Year: {character.characterdata.birth_year}
+          </h1>
+        )}
+      {planet.status === constants.STATUS_FETCHED && planet.error === null && (
+        <h1>Home Planet: {planet.planetdata.name}</h1>
+      )}
+      {film.status === constants.STATUS_FETCHED && film.error === null && (
+        <h1>First film: {film.filmdata.title}</h1>
+      )}
+    </Fragment>
+  );
+};
 
 export default App;
